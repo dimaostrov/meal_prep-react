57ba7de8dd627c405ceecb4adbe648d8f06ed471
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-11-28 18:40:38 -0800

    Updates

---
+++ precache-manifest.29b3ab014084f26bd618839595252e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e75d4c99f69ef9ed26b",
+    "url": "/meal_prep-react/static/css/main.5b20944e.chunk.css"
+  },
+  {
+    "revision": "0e75d4c99f69ef9ed26b",
+    "url": "/meal_prep-react/static/js/main.0e75d4c9.chunk.js"
+  },
+  {
+    "revision": "36aaac2a3a6bc00660ea",
+    "url": "/meal_prep-react/static/js/1.36aaac2a.chunk.js"
+  },
+  {
+    "revision": "45281aecda8e0a8456ff",
+    "url": "/meal_prep-react/static/js/runtime~main.45281aec.js"
+  },
+  {
+    "revision": "f3524729af51424c786131c5c9054046",
+    "url": "/meal_prep-react/index.html"
+  }
+];
